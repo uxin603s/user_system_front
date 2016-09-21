eb272cf7cd008333faad339d9936f53ec4168646
uxin603s <uxin603s@gmail.com>
2016-09-21 16:12:49 +0800

    更新版面配置

--- pagnation.js
+++ pagnation.js
@@ -14,14 +14,19 @@ angular.module("app").directive("pagnation",['$parse','$timeout',function($parse
 				$scope.stop_watch();
 				
 				var wacth_page_arr=function(){
-					$scope.page_arr=[];
-					if($scope.data.limit_count!=0){
-						$scope.page_count=Math.ceil($scope.data.total_count/$scope.data.limit_count);
-						
-						for(var i=0;i<$scope.page_count;i++){
-							$scope.page_arr.push(i);
+					clearTimeout($scope.wacth_page_arr_timer);
+					$scope.wacth_page_arr_timer=setTimeout(function(){
+						if($scope.data.limit_count==0){
+							$scope.page_arr=[];
+						}else{
+							$scope.page_count=Math.ceil($scope.data.total_count/$scope.data.limit_count);
+							var page_arr=[];
+							for(var i=0;i<$scope.page_count;i++){
+								page_arr.push(i);
+							}
+							$scope.page_arr=page_arr;
 						}
-					}
+					},500)
 					// console.log($scope.page_arr)
 				}
 				$scope.$watch('data.total_count',wacth_page_arr);
